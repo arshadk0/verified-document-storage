4967cff75b3bcd66caae543f94862902271d8162
Arshad Khan <arshad.k@zebpay.com>
2023-01-20 14:45:12 +0530

    contract added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import './App.css';
 import { ethers } from "ethers";
 import React, { useState } from "react";
 import Header from './components/Header';
+import contract_abi from './contract_abi.json'
+const CONTRACT_ADDRESS = "0x5c950F8CF6613779F0E897E5793b1881fE4f05eA"
 
 function App() {
   const [account, setAccount] = useState(null);
   const [provider, setProvider] = useState(null);
   const [signer, setSigner] = useState(null);
-
+  const [contract, setContract] = useState(null);
+  
   const connectWalletHandler = async () => {
     if (!window.ethereum) {
       alert("Need to install Metamask!");
@@ -18,8 +21,15 @@ function App() {
       setProvider(provider);
       const signer = provider.getSigner();
       setSigner(signer);
+      connectContract(signer)
     });
   }
+
+  const connectContract = async (signer) => {
+    const contract = new ethers.Contract(CONTRACT_ADDRESS, contract_abi, signer);
+    setContract(contract)
+  }
+
   return (
     <div className="App">
       <Header account={account} onConnect={connectWalletHandler} />

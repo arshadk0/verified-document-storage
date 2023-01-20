225d38d9c606e5e8d69a7fed0d24cc03a4404673
Arshad Khan <108132098+arshadk0@users.noreply.github.com>
2023-01-20 14:28:52 +0530

    added wallet (#5)

---
+++ src/components/Header.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Button } from "web3uikit";
+
+const header = {
+  display: "flex",
+  justifyContent: "space-between",
+  alignItems: "center",
+  padding: "1rem",
+};
+
+export default function Header({ onConnect, account }) {
+  return (
+    <div style={header}>
+      <a href="/" style={{ textDecoration: "none" }}>Verified Storage Document
+      </a>
+      <Button
+        onClick={onConnect}
+        text={!account ? "Connect Wallet" : `Connected ${account}`}
+      />
+    </div>
+  );
+}

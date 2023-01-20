dbb0155899ba8afe0938570d12fa06422a060abb
Arshad Khan <arshad.k@zebpay.com>
2023-01-20 12:47:45 +0530

    connect_wallet

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import { ethers } from "ethers";
+import React, { useState } from "react";
+import Header from './components/Header';
 
 function App() {
+  const [account, setAccount] = useState(null);
+  const [provider, setProvider] = useState(null);
+  const [signer, setSigner] = useState(null);
+
+  const connectWalletHandler = async () => {
+    if (!window.ethereum) {
+      alert("Need to install Metamask!");
+    }
+    window.ethereum.request({ method: "eth_requestAccounts" }).then((accounts) => {
+      setAccount(accounts[0]);
+      const provider = new ethers.providers.Web3Provider(window.ethereum)
+      setProvider(provider);
+      const signer = provider.getSigner();
+      setSigner(signer);
+    });
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header account={account} onConnect={connectWalletHandler} />
     </div>
   );
 }
